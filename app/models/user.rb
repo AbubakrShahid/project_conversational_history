7ebe8ec5eb93e5cf53d06ea2f6304429d96a3fbc
Abubakr Shahid <abubakarshahidd95@gmail.com>
2025-06-03 00:38:28 +0500

    Added specs and integrated docker

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { minimum: 3, maximium: 30 }
+
+  has_many :comments, dependent: :destroy
 end
